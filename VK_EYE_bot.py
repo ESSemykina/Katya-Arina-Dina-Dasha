f60c1de74eacda4ef987ea4e87cc796c88d09945
essemykina <katerinasemykina2003@gmail.com>
2023-06-28 21:42:38 +0200

    бот работает

--- VK_EYE_bot.py
+++ VK_EYE_bot.py
@@ -1,16 +1,78 @@
 import requests
+import os
+import json
 from auth_data import token_bot
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
-
 bot = Bot(token=token_bot)
 dp = Dispatcher(bot)
 
+
 @dp.message_handler(commands=["start"])
 async def start_command(message: types.Message):
-    await message.reply("Привет! Введи ФИО человека или ссылку на страницу VK")
+    await message.reply("Привет! Введи id пользователя VK")
+
+
+@dp.message_handler()
+async def get_user_data(message: types.Message):
+    access_token = "vk1.a.VZCTiIKSUiuLOxRA-ljKghO0fKVOIB7dvxvtLJ9rQ8GlKjVTJg7jn2XhA3ucwhgnYjKCVf5EdPEBgKNQ-wfWdTavYvWPU5kiu9U0uHBzJBVh7Kx6jUCEtPTUo_Du26sQSHMCYt2349usjVQ36XYrcOWJp7uSWestxyjqnz-keku3exosRCJCscRalKGNcE3So3mUudL4mjlmaaGZ-NqFeA"
+
+    try:
+        id_name = message.text
+
+        params = {
+            'user_ids': id_name,
+            'fields': 'first_name,last_name,city,country,sex,bdate',
+            'access_token': access_token,
+            'v': '5.131'
+        }
+        # URL для запроса к VK API
+        url = 'https://api.vk.com/method/users.get'
+        response = requests.get(url, params=params)
+        data = response.json()
+
+        # проверяем существует ли директория с id пользователя
+        if os.path.exists(f"{id_name}"):
+            print("", end="")
+        else:
+            os.mkdir(id_name)
+        # сохраняем данные в json файл, чтобы видеть структуру
+        with open(f"{id_name}/{id_name}.json", "w", encoding="utf-8") as file:
+            json.dump(data, file, indent=4, ensure_ascii=False)
+
+        user_info = data["response"][0]
+
+        first_name = user_info.get("first_name", "Не указано")
+        last_name = user_info.get("last_name", "Не указана")
+        bdate = user_info.get("bdate", "Не указана")
+        gender = user_info.get("sex")
+        city = user_info.get("city", {}).get("title", "Не указан")
+        country = user_info.get("country", {}).get("title", "Не указана")
+
+        if gender == 1:
+            gender = "Женский"
+        elif gender == 2:
+            gender = "Мужской"
+        else:
+            gender = "Не указан"
+
+        await message.answer(
+            f"Имя пользователя: {first_name}\n"
+            f"Фамилия пользователя: {last_name}\n"
+            f"Дата рождения: {bdate}\n"
+            f"Пол: {gender}\n"
+            f"Город: {city}\n"
+            f"Страна: {country}"
+        )
+        # Вывод данных пользователя в формате JSON
+        # print(data)
+
+
+    except Exception as e:
+        await message.reply('Произошла ошибка: ' + str(e))
+
 
 if __name__ == '__main__':
     executor.start_polling(dp)
--- main.py
+++ main.py
@@ -1,36 +1,18 @@
 import json
 import os
 import requests
+
 from auth_data import token
-import vk_api
-import vkapi
-import api
-import time
-import datetime
-
-#id_name = input("Введите название группы: ")
-#ссылка для доступа к стене в вк
-#url = f"https://api.vk.com/method/wall.get?domain={group_name}&count=5&access_token={token}&v=5.131"
-#req = requests.get(url)
-#print(req.text)
-"""vk = vk_api.VkApi(token="vk1.a.VZCTiIKSUiuLOxRA-ljKghO0fKVOIB7dvxvtLJ9rQ8GlKjVTJg7jn2XhA3ucwhgnYjKCVf5EdPEBgKNQ-wfWdTavYvWPU5kiu9U0uHBzJBVh7Kx6jUCEtPTUo_Du26sQSHMCYt2349usjVQ36XYrcOWJp7uSWestxyjqnz-keku3exosRCJCscRalKGNcE3So3mUudL4mjlmaaGZ-NqFeA")
-def getProfileInfo(id_name):
-    # получение имени и фамилии
-    user = vk.method("users.get", {"user_ids": id_name})
-    name = user[0]['first_name']
-    surname = user[0]['last_name']
-
-    print("Имя:",name,"\nФамилия:",surname)"""
 
 
 def get_user_data(id_name):
-    # Ваш access token VK API
+
     access_token = "vk1.a.VZCTiIKSUiuLOxRA-ljKghO0fKVOIB7dvxvtLJ9rQ8GlKjVTJg7jn2XhA3ucwhgnYjKCVf5EdPEBgKNQ-wfWdTavYvWPU5kiu9U0uHBzJBVh7Kx6jUCEtPTUo_Du26sQSHMCYt2349usjVQ36XYrcOWJp7uSWestxyjqnz-keku3exosRCJCscRalKGNcE3So3mUudL4mjlmaaGZ-NqFeA"
 
     # Параметры запроса
     params = {
         'user_ids': id_name,
-        'fields': 'first_name,last_name,city,country,sex',
+        'fields': 'first_name,last_name,city,country,sex,bdate',
         'access_token': access_token,
         'v': '5.131'
     }
@@ -38,13 +20,49 @@ def get_user_data(id_name):
     # URL для запроса к VK API
     url = 'https://api.vk.com/method/users.get'
 
+
+
     try:
         response = requests.get(url, params=params)
         data = response.json()
 
+        # проверяем существует ли директория с id пользователя
+        if os.path.exists(f"{id_name}"):
+            print("", end="")
+        else:
+            os.mkdir(id_name)
+        # сохраняем данные в json файл, чтобы видеть структуру
+        with open(f"{id_name}/{id_name}.json", "w", encoding="utf-8") as file:
+            json.dump(data, file, indent=4, ensure_ascii=False)
+
+
+        user_info = data["response"][0]
+        id_name=user_info.get("id")
+
+        first_name = user_info.get("first_name", "Не указано")
+        last_name = user_info.get("last_name", "Не указана")
+        bdate = user_info.get("bdate", "Не указана")
+        gender = user_info.get("sex")
+        city = user_info.get("city", {}).get("title", "Не указан")
+        country = user_info.get("country", {}).get("title", "Не указана")
+
+        if gender == 1:
+            gender = "Женский"
+        elif gender == 2:
+            gender = "Мужской"
+        else:
+            gender = "Не указан"
+
+        print("Имя пользователя:", first_name)
+        print("Фамилия пользователя:", last_name)
+        print("Дата рождения:",bdate)
+        print("Пол:", gender)
+        print("Город:", city)
+        print("Страна:", country)
+
 
         # Вывод данных пользователя в формате JSON
-        print(data)
+        #print(data)
 
 
     except Exception as e:
@@ -53,14 +71,8 @@ def get_user_data(id_name):
 
 
 def main():
-    id_name = input("Введите название группы: ")
-    #get_wall_posts(id_name)
-
-
-    # Пример использования функции
+    id_name = input("Введите id пользователя: ")
     get_user_data(id_name)
-    #getProfileInfo(id_name)
-
 
 if __name__ == '__main__':
     main()
